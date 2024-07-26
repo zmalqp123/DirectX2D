d00ad748cf2e9781fd733bb8083442df0f98f9f5
bingsub <121720685+zmalqp123@users.noreply.github.com>
2024-07-26 17:02:09 +0900

    1

--- D2DEngine/Camera.h
+++ D2DEngine/Camera.h
@@ -4,16 +4,16 @@
 #include <vector>
 class Camera : public Component
 {
-	static std::vector<Camera*> allCamera;
+	//static std::vector<Camera*> allCamera;
 public:
 	Camera() {
-		allCamera.push_back(this);
+		//allCamera.push_back(this);
 	};
 	virtual ~Camera() {
-		auto it = std::find(allCamera.begin(), allCamera.end(), this);
+		/*auto it = std::find(allCamera.begin(), allCamera.end(), this);
 		if (it != allCamera.end()) {
 			allCamera.erase(it);
-		}
+		}*/
 	};
 
 	AABB cameraRect;
